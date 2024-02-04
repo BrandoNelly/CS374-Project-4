3d5d4bc61a31277e35636049182f48cc744c46c1
root <root@DESKTOP-PV3B1OG>
2024-02-04 11:13:52 -0800

    lscw.c working as specified

--- lscw.c
+++ lscw.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+#include <string.h>
 #include <unistd.h>
 
 
-int main(int argc, char** argc) {
+int main(int argc, char* argv[]) {
 
-    
+    const char *directory = (argc == 2) ? argv[1] : ".";
 
+    int fd[2];
+    pid_t child_pid;
+
+    pipe(fd);
+
+
+    child_pid = fork();
+
+    if(child_pid == 0) {
+        dup2(fd[0], 0);
+        close(fd[1]);
+        execlp("wc", "wc", "-l", (char *)NULL);
+        exit(0);
+    } else {
+        dup2(fd[1], 1);
+        close(fd[0]);
+        execlp("ls", "ls", "-1a", directory, (char *)NULL);
+        exit(0);
+    }
 
 }
